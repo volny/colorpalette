0499f491f86671dcff0047f6925326a111cc6e5a
felix <felix@volny.co>
2016-11-22 19:46:48 +0700

    correct colors copied and events

--- app/index.js
+++ app/index.js
@@ -1,6 +1,18 @@
 import './style.scss'
 import Clipboard from 'clipboard'
 
-//new Clipboard('#btn')
-new Clipboard('.color')
+const clipboard = new Clipboard('.color')
+
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+ 
+    e.clearSelection();
+});
+ 
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
 
