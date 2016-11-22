91684f3e3c0010c2ccd1e6dbd3c20028fccef137
felix <felix@volny.co>
2016-11-22 20:08:14 +0700

    confirmation message for copy

--- app/index.js
+++ app/index.js
@@ -3,16 +3,23 @@ import Clipboard from 'clipboard'
 
 const clipboard = new Clipboard('.color')
 
-clipboard.on('success', function(e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
- 
-    e.clearSelection();
+clipboard.on('success', (event) => {
+    console.info('Action:', event.action);
+    console.info('Text:', event.text);
+    console.info('Trigger:', event.trigger);
+
+    const confirmation = document.createElement('p')
+    confirmation.className = 'confirmation'
+    confirmation.textContent = `copied ${event.text} to clipboard`
+    document.body.appendChild(confirmation)
+
+    window.setInterval(() => document.body.removeChild(confirmation), 600)
+
+    event.clearSelection();
 });
  
-clipboard.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
+clipboard.on('error', (event) => {
+    console.error('Action:', event.action);
+    console.error('Trigger:', event.trigger);
 });
 
